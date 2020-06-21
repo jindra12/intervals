a5d8ffc222d68dd8a97a8a170611cd739910e67c
Jan Jindráček <jindra12.underdark@gmail.com>
2020-06-21 02:15:41 +0200

    string testing done

---
+++ test/string/string.test.ts
@@ -0,0 +1,61 @@
+import interval from "../../src";
+
+describe("Can manipulate string intervals", () => {
+    test("Can construct default", () => {
+        const i = interval('a');
+        expect(i.has('a')).toBe(true);
+        expect(i.has('aa')).toBe(true);
+        expect(i.has('aaa')).toBe(true);
+        expect(i.has('b')).toBe(true); // if limit is infinite, all strings belong
+    });
+    test("Can construct finite interval", () => {
+        expect(interval('a', 'aaa', c => c + 'a').array()).toEqual(['a', 'aa', 'aaa']);
+    });
+    test("Move iterator", () => {
+        const i = interval('a', 'abcd', c => `${c}${String.fromCharCode(c.charCodeAt(c.length - 1) + 1)}`);
+        expect(i.val()).toBe('a');
+        expect(i.it(2).val()).toBe('abc');
+    });
+    test("Does an array contain this item? Based on limits not values", () => {
+        expect(interval('a', 'aa').has('a')).toBe(true);
+        expect(interval('a', 'aaa').has('b')).toBe(false);
+    });
+    test("Substracts interval by limits. Can return two intervals", () => {
+        expect(interval('a', 'aaaaa').diff(interval('aa', 'aaaaa'))[0].array()).toEqual(['a', 'aa']);
+        expect(interval('a', 'aa').diff(interval('aaa', 'aaaa'))[0].array()).toEqual(['a', 'aa']);
+        expect(interval('a', 'aaaaaa', c => c + 'a').diff(interval('aa', 'aaaa')).map(i => i.array())).toEqual([['a', 'aa'], ['aaaa', 'aaaaa', 'aaaaaa']]);
+    });
+    test("Concats two intervals. In non-overlapping cases, returns back both intervals", () => {
+        expect(interval('a', 'aaa', c => c + 'a').concat(interval('aa', 'aaaa', c => c + 'a'))[0].array()).toEqual(['a', 'aa', 'aaa', 'aaaa']);
+        expect(interval('a', 'aaa', c => c + 'a').concat(interval('aaa', 'aaaa', c => c + 'a'))[0].array()).toEqual(['a', 'aa', 'aaa', 'aaaa']);
+        expect(interval('a', 'aaa', c => c + 'a').concat(interval('aaa', 'aaab', c => c + 'b'))[0].array()).toEqual(['a', 'aa', 'aaa', 'aaab']);
+        expect(interval('a', 'aaa', c => c + 'a').concat(interval('aaaa', 'aaaaa', c => c + 'a')).map(i => i.array())).toEqual([['a', 'aa', 'aaa'], ['aaaa', 'aaaaa']]);
+    });
+    test("Creates copy", () => {
+        const i = interval(1, 5, c => c + 3);
+        expect(i.array()).toEqual(i.copy().array());
+    });
+    test("Do two intervals overlap?", () => {
+        expect(interval('a', 'aaa').overlap(interval('aa', 'aaaa'))).toBe(true);
+        expect(interval('a', 'aa').overlap(interval('aa', 'aaa'))).toBe(true);
+        expect(interval('a', 'aa').overlap(interval('aaa', 'aaaa'))).toBe(false);
+        expect(interval('aa', 'aaa').overlap(interval('a', 'aa'))).toBe(true);
+    });
+    test("Are the interval borders container within", () => {
+        expect(interval('aa', 'aaa').isInside(interval('aaa', 'aaaa'))).toBe(false);
+        expect(interval('a', 'aaaa').isInside(interval('aa', 'aaa'))).toBe(false);
+        expect(interval('aa', 'aaa').isInside(interval('a', 'aaaa'))).toBe(true);
+        expect(interval('a', 'aa').isInside(interval('a', 'aa'))).toBe(true);
+    });
+    test("Interval comparison, overlap: 0, greater: 1, lesser: -1", () => {
+        expect(interval('aaaa', 'aaaaa').compare(interval('a', 'aaa'))).toBe(1);
+        expect(interval('a', 'aaa').compare(interval('aa', 'aaaa'))).toBe(0);
+        expect(interval('a', 'aa').compare(interval('aa', 'aaa'))).toBe(0);
+        expect(interval('a', 'aaa').compare(interval('aaaa', 'aaaaa'))).toBe(-1);
+    });
+    test("Sorts and fills in gaps between intervals", () => {
+        expect(
+            interval('b', 'bb', c => c + 'b').fillIn([interval('aa', 'aaa', c => c + 'a'), interval('aaab', 'aaaba', c => c + 'a')]).array()
+        ).toEqual(['aa', 'aaa', 'aaab', 'aaaba']);
+    });
+});
